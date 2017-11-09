5c226d498c28fd370e7ee3416912c6c01b8929d0
anneliesvdm <annelies.vandermeulen@kuleuven.be>
2017-11-09 13:49:29 +0100

    Cleaned up example

--- modesto/Tests/example.py
+++ modesto/Tests/example.py
@@ -9,7 +9,9 @@ logging.basicConfig(level=logging.DEBUG,
                     datefmt='%m-%d %H:%M')
 logger = logging.getLogger('Main.py')
 
-# Set up Graph of network
+###########################
+# Set up Graph of network #
+###########################
 
 G = nx.DiGraph()
 
@@ -30,12 +32,18 @@ G.add_edge('p1', 'zwartbergNE', name='spZwartbergNE')
 # nx.draw(G, with_labels=True, font_weight='bold')
 # plt.show()
 
-# Set up the optimization problem
+###################################
+# Set up the optimization problem #
+###################################
 
 n_steps = 5
 time_steps = 3600
 
 modesto = Modesto(n_steps*time_steps, time_steps, 'ExtensivePipe', G)
+
+##################################
+# Fill in the parameters         #
+##################################
 
 heat_profile = pd.DataFrame([1000]*n_steps, index=range(n_steps))
 T_amb = pd.DataFrame([20+273.15]*n_steps, index=range(n_steps))
@@ -70,38 +78,65 @@ modesto.change_design_param('bbThor', 'pipe_type', 250)
 modesto.change_design_param('spWaterschei', 'pipe_type', 250)
 modesto.change_design_param('spZwartbergNE', 'pipe_type', 250)
 
+##################################
+# Solve the optimization problem #
+##################################
+
 modesto.compile()
 modesto.set_objective('energy')
 modesto.solve(tee=False)
 
-print modesto.get_result('waterscheiGarden.buildingD', 'heat_flow')
-print modesto.get_result('zwartbergNE.buildingD', 'heat_flow')
-print modesto.get_result('thorPark', 'heat_flow')
+##################################
+# Collect result                 #
+##################################
+
+print '\nWaterschei.buildingD'
+print 'Heat flow',  modesto.get_result('waterscheiGarden.buildingD', 'heat_flow')
+
+print '\nzwartbergNE.buildingD'
+print 'Heat flow', modesto.get_result('zwartbergNE.buildingD', 'heat_flow')
+
+print '\nthorPark'
+print 'Heat flow', modesto.get_result('thorPark', 'heat_flow')
 
 print '\nStorage'
-print modesto.get_result('waterscheiGarden.storage', 'heat_flow')
+print 'Heat flow', modesto.get_result('waterscheiGarden.storage', 'heat_flow')
 print 'Mass flow', modesto.get_result('waterscheiGarden.storage', 'mass_flow')
 print 'Energy', modesto.get_result('waterscheiGarden.storage', 'heat_stor')
 
+# -- Efficiency calculation --
+
+# Heat flows
 prod_hf = modesto.get_result('thorPark', 'heat_flow')
 prod_hf = [ -x for x in prod_hf]
 storage_hf = modesto.get_result('waterscheiGarden.storage', 'heat_flow')
 waterschei_hf = modesto.get_result('waterscheiGarden.buildingD', 'heat_flow')
 zwartberg_hf = modesto.get_result('zwartbergNE.buildingD', 'heat_flow')
 
-fig, ax = plt.subplots()
+# Sum of heat flows
+prod_e = sum(prod_hf)
+storage_e = sum(storage_hf)
+waterschei_e = sum(waterschei_hf)
+zwartberg_e = sum(zwartberg_hf)
 
-ax.hold(True)
-l1,=ax.plot(prod_hf)
-l3,=ax.plot([x + y + z for x, y, z in zip(waterschei_hf, zwartberg_hf, storage_hf)])
-ax.axhline(y=0, linewidth=2, color='k', linestyle='--')
+# Efficiency
+print '\nNetwork'
+print 'Efficiency', (storage_e + waterschei_e + zwartberg_e)/prod_e*100, '%'
 
-ax.set_title('Heat flows [W]')
 
-fig.legend((l1, l3),
-           ('Producer',
-            'Users and storage'),
-            'lower center', ncol=3)
-
-plt.show()
+# fig, ax = plt.subplots()
+#
+# ax.hold(True)
+# l1,=ax.plot(prod_hf)
+# l3,=ax.plot([x + y + z for x, y, z in zip(waterschei_hf, zwartberg_hf, storage_hf)])
+# ax.axhline(y=0, linewidth=2, color='k', linestyle='--')
+#
+# ax.set_title('Heat flows [W]')
+#
+# fig.legend((l1, l3),
+#            ('Producer',
+#             'Users and storage'),
+#             'lower center', ncol=3)
+#
+# plt.show()
 
