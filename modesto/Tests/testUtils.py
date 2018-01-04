d803d8cf05559ac0c9469e8204a4abac9a6bb0f8
Bram van der Heijde <b.vanderh@hotmail.com>
2018-01-04 17:22:37 +0100

    read timestamps in simpler way. Test utils

---
+++ modesto/Tests/testUtils.py
@@ -0,0 +1,21 @@
+import modesto.utils as ut
+from pkg_resources import resource_filename
+import matplotlib.pyplot as plt
+
+path = resource_filename('modesto', 'Data/RenewableProduction')
+test = ut.read_time_data(path, 'SolarThermal.txt')
+
+print test.head()
+
+test_res = ut.resample(test, new_sample_time=3600)
+print test_res.head()
+
+fig, axs = plt.subplots(2,1)
+
+axs[0].plot(test[:'20140104'])
+axs[1].plot(test_res[:'20140104'])
+
+plt.show()
+
+
+
--- modesto/utils.py
+++ modesto/utils.py
@@ -7,12 +7,13 @@ import os.path
 import pandas as pd
 
 
-def read_file(path, name):
+def read_file(path, name, timestamp=True):
     """
     Read a text file and return it as a dataframe
 
     :param path: Location of the file
     :param name: name of the file (add extension)
+    :param timestamp: if data contains a timestamp column. Default True
     :return: A dataframe
     """
 
@@ -21,7 +22,8 @@ def read_file(path, name):
     if not os.path.isfile(fname):
         raise IOError(fname + ' does not exist')
 
-    data = pd.read_csv(fname, sep=" ", header=None)
+    data = pd.read_csv(fname, sep=" ", header=0, parse_dates=timestamp,
+                       index_col=0)
 
     return data
 
@@ -40,17 +42,12 @@ def read_time_data(path, name):
 
     df = read_file(path, name)
 
-    df.columns = df.ix[0, :]
-    df = df.drop(df.index[0])
-    df.ix[:, 0] = pd.to_datetime(df.ix[:, 0])
-    df.index = df.ix[:, 0]
-    df = df.drop(df.columns[0], axis=1)
     df = df.astype('float')
 
     return df
 
 
-def resample(old_data, old_sample_time, new_sample_time, method=None):
+def resample(old_data, new_sample_time, old_sample_time=None, method=None):
     """
     Resamples data
     :param old_data: A data frame, containing the time data
@@ -59,6 +56,9 @@ def resample(old_data, old_sample_time, new_sample_time, method=None):
     :param method: The method resampling to be used (sum/mean)
     :return: The resampled dataFrame
     """
+
+    if old_sample_time is None:
+        old_sample_time = (old_data.index[1]-old_data.index[0]).seconds
 
     if (new_sample_time == old_sample_time) or (new_sample_time is None):
         return old_data
