ca9fc926377db253da4653f082b94f1be5d6ae43
Bram van der Heijde <bram.vanderheijde@kuleuven.be>
2018-03-07 14:31:26 +0100

    Change setup.py and add setup.cfg

--- setup.py
+++ setup.py
@@ -6,6 +6,8 @@ setup(
     packages=find_packages(),
     package_dir={'modesto': 'modesto'},
     package_data={'': ['*.txt', '*.rst', '*.csv']},
+    setup_requires=['pytest-runner'],
+    tests_require=['pytest'],
     url='',
     license='',
     author='Annelies Vandermeulen and Bram van der Heijde',
