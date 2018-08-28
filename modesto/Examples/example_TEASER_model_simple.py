79d34cc3322d31f8cc6594bcc728d958703456f1
Bram van der Heijde <bram.vanderheijde@kuleuven.be>
2018-08-28 11:30:53 +0200

    Reformat

--- modesto/Examples/example_TEASER_model_simple.py
+++ modesto/Examples/example_TEASER_model_simple.py
@@ -19,8 +19,8 @@ logger = logging.getLogger('Main.py')
 # Set up Graph of network #
 ###########################
 
-time_step = 300
-n_steps = 24 * 7 * int(3600 / time_step)
+time_step = 60
+n_steps = 24 * 3 * int(3600 / time_step)
 
 start_time = pd.Timestamp('20141231')
 
@@ -267,5 +267,6 @@ if __name__ == '__main__':
 
     fig, ax = plt.subplots(1, 1)
     ax.plot(TiD_ws)
+    ax.plot(TiD_ws_2)
 
     plt.show()
