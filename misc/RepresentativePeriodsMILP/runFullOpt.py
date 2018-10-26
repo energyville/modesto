5f73c059fec5d4697e8ed8829c785b4a13f29e02
Bram van der Heijde <bram.vanderheijde@kuleuven.be>
2018-10-26 17:23:13 +0200

    Add new design parameter values

--- misc/RepresentativePeriodsMILP/runFullOpt.py
+++ misc/RepresentativePeriodsMILP/runFullOpt.py
@@ -20,8 +20,8 @@ df = pd.DataFrame(
              'E_loss_stor_full',
              'E_curt_full', 'E_sol_full', 'E_net_loss_full', 't_full'])
 
-for VWat in [75000, 100000, 125000]:
-    for A in [50000, 100000, 150000]:  # , 60000, 80000]:
+for VWat in [50000, 75000, 100000, 125000]:
+    for A in [25000, 50000, 75000]:  # , 60000, 80000]:
         for VSTC in [100000, 150000, 200000]:  # , 3.85e6, 4.1e6, 4.35e6, 4.6e6]:
             print 'A:', str(A)
             print 'VWat:', str(VWat)
--- misc/RepresentativePeriodsMILP/testPostPr.py
+++ misc/RepresentativePeriodsMILP/testPostPr.py
@@ -7,7 +7,7 @@ import seaborn as sns
 
 sns.set_style('darkgrid')
 
-for path in ['3dnewsol', '7dnewsol']: # '1dnewsol',
+for path in ['1dnewsol', '7dnewsol', '3dnewsol']: #  , '7dnewsol']: # ,
     filepath = os.path.join('../RepresentativePeriodsMILP/results', path)
     for filename in os.listdir(filepath):
         if not filename == 'summary.txt':
@@ -39,7 +39,12 @@ for path in ['3dnewsol', '7dnewsol']: # '1dnewsol',
             data[['A', 'VWat', 'VSTC']] = data[['A', 'VWat', 'VSTC']].astype(int)
             data = data.rename(columns={'A': 'Solar coll. area', 'VWat': 'Storage Wat.', 'VSTC': 'Storage STC'})
 
-            resultname = 'loss_stor'
+
+            ##################
+            resultname = 'sol'
+            ##################
+
+
             fullname = 'E_{}_full'.format(resultname)
             reprname = 'E_{}_repr'.format(resultname)
 
@@ -69,8 +74,8 @@ for path in ['3dnewsol', '7dnewsol']: # '1dnewsol',
 
 
 
-                ax.set_xlim(xlim)
-                ax.set_ylim(ylim)
+                ax.set_xlim(limmin, limmax)
+                ax.set_ylim(limmin, limmax)
 
             g.add_legend(title='Storage volume', bbox_to_anchor=(1, 0.5))
 
